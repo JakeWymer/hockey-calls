6791f84704258861949a85a7c732d17fe939a24c
Jake Wymer <jakewymer@att.net>
2016-12-14 16:13:32 -0600

    add favicon and fix picks route

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Laravel</title>
+        <title>Stars Calls</title>
+
+         <link rel="shortcut icon" href="{{{ asset('assets/favicon.ico') }}}">
 
         <!-- Latest compiled and minified CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
